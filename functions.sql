28202988375d6d78ac10312edde69584d80bb865
Anuka Hettiarachchi <anuka3382@gmail.com>
2024-09-04 21:43:54 +0530

    get total sum of product and customer sales ytd

--- functions.sql
+++ functions.sql
@@ -477,3 +477,67 @@ EXCEPTION
         ROLLBACK;
 END ADD_SIMPLE_SALE_VIASQLDEV;
 /
+
+-- get total sum of customer sales ytd
+CREATE OR REPLACE FUNCTION SUM_CUST_SALESYTD
+RETURN NUMBER
+AS
+    total_sales NUMBER;
+BEGIN
+    SELECT NVL(SUM(SALES_YTD), 0) INTO total_sales FROM CUSTOMER;
+    RETURN total_sales;
+
+EXCEPTION
+    WHEN OTHERS THEN
+        RAISE_APPLICATION_ERROR(-20000, 'Error: ' || SQLERRM);
+END SUM_CUST_SALESYTD;
+/
+
+-- get total sum of customer sales ytd as dev
+CREATE OR REPLACE PROCEDURE SUM_CUST_SALES_VIASQLDEV
+AS
+    total_sales NUMBER;
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('--------------------------------------------');
+    DBMS_OUTPUT.PUT_LINE('Summing Customer SalesYTD');
+    total_sales := SUM_CUST_SALESYTD;
+
+    DBMS_OUTPUT.PUT_LINE('All Customer Total: ' || TO_CHAR(total_sales, '99999.99'));
+
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error: ' || SQLERRM);
+END SUM_CUST_SALES_VIASQLDEV;
+/
+
+-- get total sum of product sales ytd
+CREATE OR REPLACE FUNCTION SUM_PROD_SALESYTD_FROM_DB
+RETURN NUMBER
+AS
+    total_sales NUMBER;
+BEGIN
+    SELECT NVL(SUM(SALES_YTD), 0) INTO total_sales FROM PRODUCT;
+    RETURN total_sales;
+
+EXCEPTION
+    WHEN OTHERS THEN
+        RAISE_APPLICATION_ERROR(-20000, 'Error: ' || SQLERRM);
+END SUM_PROD_SALESYTD_FROM_DB;
+/
+
+-- get total sum of product sales ytd as dev
+CREATE OR REPLACE PROCEDURE SUM_PROD_SALES_VIASQLDEV
+AS
+    total_sales NUMBER;
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('--------------------------------------------');
+    DBMS_OUTPUT.PUT_LINE('Summing Product SalesYTD');
+
+    total_sales := SUM_PROD_SALESYTD_FROM_DB;
+
+    DBMS_OUTPUT.PUT_LINE('All Product Total: ' || TO_CHAR(total_sales, '99999.99'));
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error: ' || SQLERRM);
+END SUM_PROD_SALES_VIASQLDEV;
+/
