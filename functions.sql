e1d786efe6b979ec1fa5dbc7b4f7a4a5b28c8d26
Anuka Hettiarachchi <anuka3382@gmail.com>
2024-09-04 21:29:31 +0530

    get custom string and sales ytd from database

--- functions.sql
+++ functions.sql
@@ -181,3 +181,95 @@ EXCEPTION
   
 END DELETE_ALL_PRODUCTS_VIASQLDEV;
 /
+
+--get custom string from database
+CREATE OR REPLACE FUNCTION GET_CUST_STRING_FROM_DB (
+    pcustid NUMBER
+)
+RETURN VARCHAR2
+AS
+    name VARCHAR2(50);
+    status VARCHAR2(20);
+    sales_ytd NUMBER(10, 2);
+
+BEGIN
+    SELECT CUSTOMER.CUSTNAME, CUSTOMER.STATUS, CUSTOMER.SALES_YTD
+    INTO name, status, sales_ytd
+    FROM CUSTOMER
+    WHERE CUSTID = pcustid;
+
+    RETURN 'Custid: ' || pcustid || ' Name: ' || name || ' Status: ' || status || ' SalesYTD: ' || TO_CHAR(sales_ytd, '999.99');
+
+EXCEPTION
+
+    WHEN NO_DATA_FOUND THEN
+        RAISE_APPLICATION_ERROR(-20061, 'Customer ID not found');
+    
+    WHEN OTHERS THEN
+        RAISE_APPLICATION_ERROR(-20000, 'Error: ' || SQLERRM);
+END GET_CUST_STRING_FROM_DB;
+/
+
+--get custom string from database as dev
+CREATE OR REPLACE PROCEDURE GET_CUST_STRING_VIASQLDEV (
+    pcustid NUMBER
+)
+AS
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('------------------------------');
+    DBMS_OUTPUT.PUT_LINE('Getting Details for CustId ' || pcustid);
+    DBMS_OUTPUT.PUT_LINE(GET_CUST_STRING_FROM_DB(pcustid));
+
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE(SQLERRM);
+END GET_CUST_STRING_VIASQLDEV;
+/
+
+--get custom sales ytd from database
+CREATE OR REPLACE PROCEDURE UPD_CUST_SALESYTD_IN_DB (
+    pcustid NUMBER,
+    pamt NUMBER
+)
+AS
+    rows_updated NUMBER;
+BEGIN
+    IF pamt < -999.99 OR pamt > 999.99 THEN
+        RAISE_APPLICATION_ERROR(-20083, 'Amount out of range');
+    END IF;
+
+    UPDATE CUSTOMER
+    SET sales_ytd = sales_ytd + pamt
+    WHERE custid = pcustid;
+
+    rows_updated := SQL%ROWCOUNT;
+
+    IF rows_updated = 0 THEN
+        RAISE_APPLICATION_ERROR(-20071, 'Customer ID not found');
+    END IF;
+
+EXCEPTION
+    WHEN OTHERS THEN
+        RAISE_APPLICATION_ERROR(-20000, 'Unexpected Error: ' || SQLERRM);
+END UPD_CUST_SALESYTD_IN_DB;
+/
+
+--get custom sales ytd from database as dev
+CREATE OR REPLACE PROCEDURE UPD_CUST_SALESYTD_VIASQLDEV (
+    pcustid NUMBER,
+    pamt NUMBER
+)
+AS
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('--------------------------------------------');
+    DBMS_OUTPUT.PUT_LINE('Updating SalesYTD. Customer Id: ' || pcustid || ' Amount: ' || TO_CHAR(pamt, '999.99'));
+    UPD_CUST_SALESYTD_IN_DB(pcustid, pamt);
+
+    DBMS_OUTPUT.PUT_LINE('Update OK');
+    COMMIT;
+
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error: ' || SQLERRM);
+END UPD_CUST_SALESYTD_VIASQLDEV;
+/
